e90f9e23364bffe8f49946a9bc943a9350d1b407
zendau <cowalyow2012@gmail.com>
2023-07-23 19:18:33 +0300

    VDOM render

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,67 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type eventTypes = "click" | "input";
+type eventAction = [eventTypes, () => void];
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface IVDOMElement {
+  value?: string;
+  id?: string;
+  class?: string[];
+  tag: string;
+  children?: IVDOMElement[];
+  dataset?: Record<string, any>;
+  events?: eventAction[];
+}
+
+function testEvent() {
+  console.log("click test");
+}
+
+const vDOME: IVDOMElement = {
+  tag: "div",
+  class: ["box"],
+  children: [
+    {
+      value: "test element",
+      tag: "p",
+      class: ["red"],
+    },
+  ],
+  dataset: {
+    count: 5,
+  },
+  events: [["click", testEvent]],
+};
+
+function render(root: HTMLElement | null, vdome: IVDOMElement) {
+  if (!root) return;
+
+  const el = document.createElement(vdome.tag);
+
+  if (vdome.id) {
+    el.id = vdome.id;
+  }
+
+  if (vdome.class) {
+    el.classList.add(...vdome.class);
+  }
+
+  if (vdome.value) {
+    el.innerText = vdome.value;
+  }
+
+  if (vdome.children) {
+    vdome.children.forEach((child) => render(el, child));
+  }
+
+  if (vdome.events) {
+    vdome.events.forEach((event) => {
+      el.addEventListener(event[0], event[1]);
+    });
+  }
+
+  root.appendChild(el);
+}
+
+const appRoot = document.getElementById("app");
+render(appRoot, vDOME);
