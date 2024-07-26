9f456c1df92720d8942e0302a5c26c67f6fe65bb
Zendau <cowalyow2012@gmail.com>
2024-07-27 00:22:53 +0300

    refactor(core): remove deprecated experimental debounce for fixing multiple renders

--- packages/core/src/utils/debounce.ts
+++ packages/core/src/utils/debounce.ts
@@ -1,24 +1,10 @@
 export default function debounce(func: any, delay: any) {
   let timerId: any;
 
-  let prevReactive: any = null
-
   return function (...args: any) {
-
-    console.log('CALL DEBAUNCE')
-
-    if (args[0] === prevReactive) {
-      debugger
-      console.log("REACTIVE COPY")
-    } else {
-      prevReactive = args[0]
-    }
-
-    console.log('args', args)
-    func(...args)
-    // clearTimeout(timerId);
-    // timerId = setTimeout(() => {
-    //   func(...args);
-    // }, delay);
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func(...args);
+    }, delay);
   };
 }
